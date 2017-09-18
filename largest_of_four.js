e4a6a6193ad6066248693aa09ddc7acd58b2c9af
doctorpacket <gottingerfrank@zoho.com>
2017-09-18 03:12:55 +0200

    changed comments

---
+++ largest_of_four.js
@@ -0,0 +1,13 @@
+// Return an array consisting of the largest number from each provided sub-array.
+// For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+// Remember, you can iterate through an array with a simple for loop,
+// and access each member with array syntax arr[i].
+
+
+function largestOfFour(arr) {
+  // You can do this!
+  return arr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
--- titlecase.js
+++ titlecase.js
@@ -1,8 +1,8 @@
-/*Return the provided string with the first letter of each word capitalized.
-Make sure the rest of the word is in lower case.
+// Return the provided string with the first letter of each word capitalized.
+// Make sure the rest of the word is in lower case.
 
-For the purpose of this exercise,
-you should also capitalize connecting words like "the" and "of".*/
+// For the purpose of this exercise,
+// you should also capitalize connecting words like "the" and "of".
 
 
 function titleCase(str) {
