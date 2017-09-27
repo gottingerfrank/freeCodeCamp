bcd05d976d14764888b096d60eeb55e522a683ab
doctorpacket <gottingerfrank@zoho.com>
2017-09-27 03:25:25 +0200

    added slasher

---
+++ slasher.js
@@ -0,0 +1,11 @@
+// Return the remaining elements of an array after
+// chopping off n elements from the head.
+
+function slasher(arr, howMany) {
+  for (var i=0; i<howMany; i++) {
+    arr.shift();
+  }
+  return arr;
+}
+
+slasher([1, 2, 3], 2);
